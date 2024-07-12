c52d276bad1e3cdc9c2760653ea16796709d6909
Delon00 <djplebob32@gmail.com>
2024-07-12 23:30:09 +0000

    modified:   app/globals.css 	modified:   app/layout.tsx 	modified:   app/page.tsx 	new file:   components/cards/cardFramework.jsx 	new file:   components/cards/styleCard.css 	new file:   components/navbar/navbar.css 	new file:   components/navbar/navbar.jsx 	modified:   package-lock.json 	modified:   package.json 	new file:   public/card.png 	new file:   public/date-picker.png 	new file:   public/expo.png 	new file:   public/formlog.png 	new file:   public/guapo.png 	new file:   public/laravel.png 	new file:   public/mobile.png 	new file:   public/nextjs.png 	new file:   public/react.png 	new file:   public/tabs.png 	new file:   public/volume.png 	new file:   public/vscode.png 	modified:   tailwind.config.ts

---
+++ components/cards/cardFramework.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import "./styleCard.css";
+
+export default function Card({ children }) {
+    return <main className="card-framework">{children}</main>;
+}
---
+++ components/navbar/navbar.jsx
@@ -0,0 +1,39 @@
+'use client'
+import React, { useState, useEffect } from "react";
+import "./navbar.css"
+
+export default function Navbar(){
+
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 100) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return(
+        <main className={`navbar ${isScrolled ? "scroll" : ""}`}>
+            <h3>Devcci</h3>
+            <div className="nav-search">
+                <input className="nav-input" type="text" />
+                <img width="15" height="15" src="https://img.icons8.com/ios/50/search--v1.png" alt="search--v1"/>
+            </div>
+            <div className="navlinks">
+                <a className="nav-link" href="/composant">Docs</a>
+                <a className="nav-link" href="/composant">Composants</a>
+                <a className="nav-link" href="/composant">Astuces</a>
+            </div>
+        </main>
+    )
+}
